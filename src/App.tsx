39463e9199141de0e09a2afe06907c827066f461
Dongmin,Yoo <ydm2790@gmail.com>
2023-03-04 16:21:51 +0900

    feat(android): ✨ created feature that toggle between sensors

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,124 @@
 import React, { useEffect, useState } from 'react';
-import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Platform,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {
   isStepCountingSupported,
   parseStepData,
   startStepCounterUpdate,
   stopStepCounterUpdate,
 } from '@dongminyu/react-native-step-counter';
-import { requestPermission } from './permission';
+import {
+  getBodySensorPermission,
+  getStepCounterPermission,
+} from './permission';
+
+type SensorType<T = typeof Platform.OS> = T extends 'ios'
+  ? 'CMStepCounter' | 'CMPedometer'
+  : T extends 'android'
+  ? 'STEP_COUNTER' | 'ACCELEROMETER'
+  : 'NONE';
+
+type SensorName = SensorType<Platform['OS']>;
+
+const initState = {
+  dailyGoal: '0/10000 steps',
+  stepsString: '0.0kCal',
+  calories: '0 steps',
+  distance: '0.0m',
+};
 
 export default function App() {
   const [supported, setSupported] = useState(false);
   const [granted, setGranted] = useState(false);
-  const [additionalInfo, setAdditionalInfo] = useState({
-    dailyGoal: '0/10000 steps',
-    stepsString: '0.0kCal',
-    calories: '0 steps',
-    distance: '0.0m',
-  });
+  const [sensorType, setSensorType] = useState<SensorName>('NONE');
+  const [additionalInfo, setAdditionalInfo] = useState(initState);
 
   /** get user's motion permission and check pedometer is available */
-  async function askPermission() {
+  const isPedometerSupported = () => {
     isStepCountingSupported().then(result => {
-      console.debug('🚀 - isStepCountingSupported', result);
       setGranted(result.granted === true);
       setSupported(result.supported === true);
     });
-  }
+  };
 
-  async function startStepCounter() {
+  const startStepCounter = () => {
     startStepCounterUpdate(new Date(), data => {
+      console.debug('🚀 data', data);
+      setSensorType(data.counterType as SensorName);
+      const parsedData = parseStepData(data);
       setAdditionalInfo({
-        ...parseStepData(data),
+        ...parsedData,
       });
     });
-  }
+  };
 
-  function stopStepCounter() {
-    setAdditionalInfo({
-      dailyGoal: '0/10000 steps',
-      stepsString: '0.0kCal',
-      calories: '0 steps',
-      distance: '0.0m',
-    });
+  const stopStepCounter = () => {
+    setAdditionalInfo(initState);
     stopStepCounterUpdate();
-  }
+  };
+
+  const forceUseAnotherSensor = () => {
+    if (sensorType !== 'NONE') {
+      stopStepCounter();
+    }
+    if (sensorType === 'STEP_COUNTER') {
+      getBodySensorPermission().then(setGranted);
+    } else {
+      getStepCounterPermission().then(setGranted);
+    }
+    startStepCounter();
+  };
 
   useEffect(() => {
-    console.debug('🚀 - componentDidMount');
-    askPermission();
+    isPedometerSupported();
     return () => {
-      console.debug('🚀 - componentWillUnmount');
       stopStepCounter();
     };
   }, []);
 
   useEffect(() => {
-    console.debug('🚀 - componentDidUpdate');
-    if (granted && supported) {
-      console.debug('🚀 - granted and supported');
-      startStepCounter();
-    } else if (granted && !supported) {
-      console.debug('🚀 - granted but not supported');
-      startStepCounter();
-    } else if (supported && !granted) {
-      console.debug('🚀 - supported but not granted');
-      requestPermission().then(accepted => {
-        console.debug('🚀 - requestPermission', accepted);
-        setGranted(accepted);
-      });
-    }
+    console.debug(`🚀 stepCounter ${supported ? '' : 'not'} supported`);
+    console.debug(`🚀 user ${granted ? 'granted' : 'denied'} stepCounter`);
+    startStepCounter();
   }, [granted, supported]);
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
         <Text style={styles.normText}>
-          User Granted Step Counter Feature?: {granted ? 'yes' : 'no'}
+          User Granted the Permission?: {granted ? 'yes' : 'no'}
         </Text>
         <Text style={styles.normText}>
-          Device has Step Counter Sensor?: {supported ? 'yes' : 'no'}
+          Device has Pedometer Sensor?: {supported ? 'yes' : 'no'}
         </Text>
-        {!granted ? (
-          <>
-            <Button
-              title="Request Permission Again"
-              onPress={requestPermission}
-            />
-          </>
+        {Platform.OS === 'android' ? (
+          <Button
+            title={`sensor: ${sensorType}`}
+            onPress={forceUseAnotherSensor}
+          />
         ) : (
-          <>
-            <Text style={styles.normText}>
-              dailyGoal : {additionalInfo.dailyGoal}
-            </Text>
-            <Text style={styles.normText}>
-              calories : {additionalInfo.calories}
-            </Text>
-            <Text style={styles.normText}>
-              stepsString : {additionalInfo.stepsString}
-            </Text>
-            <Text style={styles.normText}>
-              distance : {additionalInfo.distance}
-            </Text>
-            <Button
-              title="Start StepCounter Update"
-              onPress={startStepCounter}
-            />
-            <Button
-              title="Stop StepCounter Updates"
-              onPress={stopStepCounter}
-            />
-          </>
+          <Text style={styles.normText}>now Using : {sensorType}</Text>
         )}
+        <Text style={styles.normText}>
+          dailyGoal : {additionalInfo.dailyGoal}
+        </Text>
+        <Text style={styles.normText}>
+          calories : {additionalInfo.calories}
+        </Text>
+        <Text style={styles.normText}>
+          stepsString : {additionalInfo.stepsString}
+        </Text>
+        <Text style={styles.normText}>
+          distance : {additionalInfo.distance}
+        </Text>
+        <Button title="Start StepCounter Update" onPress={startStepCounter} />
+        <Button title="Stop StepCounter Updates" onPress={stopStepCounter} />
       </View>
     </SafeAreaView>
   );
--- src/permission.ts
+++ src/permission.ts
@@ -1,29 +1,59 @@
 import {
-  Permission,
+  check,
+  openSettings,
+  type Permission,
+  type PermissionStatus,
   PERMISSIONS,
   request,
   RESULTS,
 } from 'react-native-permissions';
-import { Platform, Rationale } from 'react-native';
+import { Platform, type Rationale } from 'react-native';
+import appInformation from '../package.json';
 
-export const requestPermission = async () => {
-  const permission: Permission =
-    Platform.OS === 'ios'
-      ? PERMISSIONS.IOS.MOTION
-      : PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION;
-  const rationale: Rationale = {
-    title: '"Step Counter" Permission',
-    message: '"Step Counter" needs access to your sensor data.',
+const bodySensor = PERMISSIONS.ANDROID.BODY_SENSORS_BACKGROUND;
+const activityRecognition = PERMISSIONS.ANDROID.ACTIVITY_RECOGNITION;
+const motion = PERMISSIONS.IOS.MOTION;
+
+const CHECK = <S = PermissionStatus>(result: S) => result === RESULTS.GRANTED;
+
+const getRational = (permission: Permission): Rationale => {
+  let data = '';
+  if (permission === activityRecognition) {
+    data = 'Step Counter';
+  } else if (permission === bodySensor) {
+    data = 'Accelerometer';
+  } else if (permission === motion) {
+    data = 'Core Motion Pedometer';
+  }
+  const appName = appInformation.name;
+  return {
+    title: `"${data}" Permission`,
+    message: `"${appName}" needs access to your ${data.toLowerCase()} data.`,
     buttonPositive: 'ACCEPT',
     buttonNegative: 'DENY',
   };
-  return request(permission, rationale).then(result => {
-    if (result === RESULTS.GRANTED) {
-      console.debug('The permission is granted');
-      return true;
-    } else {
-      console.debug('The permission is denied');
-      return false;
-    }
-  });
+};
+
+const requestPermission = async (permission: Permission) => {
+  const rationale = getRational(permission);
+  return request(permission, rationale).then(CHECK);
+};
+
+const checkPermission = async (permission: Permission) => {
+  return check(permission).then(CHECK);
+};
+
+export const getStepCounterPermission = async () => {
+  const permission: Permission =
+    Platform.OS === 'ios' ? motion : activityRecognition;
+  if (await requestPermission(permission)) {
+    return true;
+  }
+  openSettings();
+  return checkPermission(permission);
+};
+
+export const getBodySensorPermission = async () => {
+  await openSettings();
+  return checkPermission(bodySensor);
 };
