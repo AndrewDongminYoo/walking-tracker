3266f2ec2b90a23af43ea47e4958c68ccaa51e7f
Dongmin Yu <ydm2790@gmail.com>
2023-10-30 22:07:25 +0900

    chore(react): ⬆️ bump packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 'off',
+    'react-native/no-inline-styles': 'off',
+  },
+  ignorePatterns: ['!.prettierrc.js'],
 };
